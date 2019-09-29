4f2958762917e416c232844e9bb85815efb167c5
Danny <salas.danny2013@gmail.com>
2019-09-29 18:40:42 -0500

    mde minor changes

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000;
-
 // Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
@@ -26,4 +24,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
